131524752cc024b3b7ebf17f8743de2b721e80f3
Tawishi <tawishisharma1@gmail.com>
2020-12-09 19:19:44 +0530

    Add for loop in C

---
+++ for_loop.c
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+void main() {
+    for(int i=1;i<6;i++)
+        printf("%d\n",i);
+    printf("\n");
+    int numbers[] = {10,15,21,20,7,3,5};
+    for(int i=0;i<7;i++)
+        printf("%d\n",numbers[i]); 
+}
