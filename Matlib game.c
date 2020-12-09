e29f1a713b7f0ba74c0b717fdbe8c623698ac574
Tawishi <tawishisharma1@gmail.com>
2020-12-09 13:16:50 +0530

    Modify for full name in Matlib game

--- Matlib game.c
+++ Matlib game.c
@@ -1,15 +1,15 @@
 #include<stdlib.h>
 #include<stdio.h>
 void main() {
-    char color[20],plural[20],celeb[50];
+    char color[20],plural[20],celebF[20], celebL[20];
     printf("Enter a color : ");
     scanf("%s", color);
     printf("Enter a plural noun : ");
     scanf("%s", plural);
     printf("Enter a celebrity : ");
-    scanf("%s", celeb);
+    scanf("%s%s", celebF, celebL);
 
-    printf("Roeses are %s\n",color);
+    printf("Roses are %s\n",color);
     printf("%s are blue\n",plural);
-    printf("I love %s\n",celeb);
+    printf("I love %s %s\n",celebF, celebL);
 }
